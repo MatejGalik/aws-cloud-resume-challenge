d54328fd677cf4ea51e8530b98f3982be0772e43
Matej Galik <matogalik6@gmail.com>
2025-08-30 23:10:22 +0200

    initial commit

---
+++ website/index.js
@@ -0,0 +1,9 @@
+const counter = document.querySelector(".resume_counter");
+
+async function updateCounter() {
+    let response = await fetch("https://fiv6llwfaaelywbvirygwsgpg40aqago.lambda-url.us-east-1.on.aws/");
+    let data = await response.json();
+    counter.innerHTML = `Views: ${data}`;  // ← BACKTICKS, nie apostrofy!
+}
+
+updateCounter();
