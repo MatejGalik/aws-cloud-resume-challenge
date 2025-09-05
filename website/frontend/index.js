31b68cea9f9b428d4fadb0c08aef969e8daa8853
Matej Galik <matogalik6@gmail.com>
2025-09-05 16:28:34 +0200

    Adding backend cicd pipeline-test2

--- website/frontend/index.js
+++ website/frontend/index.js
@@ -3,7 +3,7 @@ const counter = document.querySelector(".resume_counter");
 async function updateCounter() {
     let response = await fetch("https://fiv6llwfaaelywbvirygwsgpg40aqago.lambda-url.us-east-1.on.aws/");
     let data = await response.json();
-    counter.innerHTML = `Views: ${data}`;  // ← BACKTICKS, nie apostrofy!
+    counter.innerHTML = `Views: ${data}`; 
 }
 
 updateCounter();
