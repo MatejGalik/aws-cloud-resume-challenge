3a53ec2b0fe392c3bee36f2b27c05088cad496dc
Matej Galik <matogalik6@gmail.com>
2025-09-16 13:20:34 +0200

    New changes in html and css

--- website/frontend/index.js
+++ website/frontend/index.js
@@ -3,7 +3,7 @@ const counter = document.querySelector(".resume_counter");
 async function updateCounter() {
     let response = await fetch("https://fiv6llwfaaelywbvirygwsgpg40aqago.lambda-url.us-east-1.on.aws/");
     let data = await response.json();
-    counter.innerHTML = `Views: ${data}`; 
+    counter.innerHTML = `<strong>Views: </strong> ${data}`; 
 }
 
 updateCounter();
